95662e17106ca0af1ade13306ff1ad567c1c14a8
Dhanasekar <sendmailtodhana@gmail.com>
2019-12-18 21:46:22 +0530

    background alone

--- lib/screen/login_screen.dart
+++ lib/screen/login_screen.dart
@@ -1,8 +1,14 @@
 import 'package:flutter/material.dart';
 
-class LoginScreen extends StatelessWidget{
- @override
-  Widget build(BuildContext context){
-    return MaterialApp();
+class LoginScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(23, 66, 64, 1.0),
+      body: Container(
+        child: Text("Hi Welcome!",
+            style: TextStyle(color: Colors.white, fontSize: 20)),
+      ),
+    );
   }
 }
